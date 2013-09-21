494c8657e735e1a1f0ccb926f2fa93f4c6f1dfcb
Malcolm McKinney <mim46@rrdhcp-10-32-248-193.redrover.cornell.edu>
2013-09-21 08:47:29 -0400

    Sample Files for Objective-C API

---
+++ ShareFileSample/Sample.h
@@ -0,0 +1,13 @@
+//
+//  Sample.h
+//  ShareFileSample
+//
+//  Created by Malcolm McKinney on 8/1/13.
+//  Copyright (c) 2013 Malcolm McKinney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sample : NSObject
+
+@end
---
+++ ShareFileSample/Sample.m
@@ -0,0 +1,13 @@
+//
+//  Sample.m
+//  ShareFileSample
+//
+//  Created by Malcolm McKinney on 8/1/13.
+//  Copyright (c) 2013 Malcolm McKinney. All rights reserved.
+//
+
+#import "Sample.h"
+
+@implementation Sample
+
+@end
